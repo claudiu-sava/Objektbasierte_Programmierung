9161907f4d6dc2f280a3f73899e9453928c97363
claudiu-sava <claudiu.sava@protonmail.com>
2023-08-21 14:47:51 +0200

    idk

---
+++ Aufgabe Funktionen/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Aufgabe_Funktionen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MyMath.calc_ggT(2,3));
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿9class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
