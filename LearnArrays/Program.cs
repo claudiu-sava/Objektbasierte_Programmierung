e763227fb957a66f39e17d09f9da9a1bae8d75be
claudiu-sava <claudiu.sava@protonmail.com>
2023-08-21 15:20:38 +0200

    Add Working Microsoft Challenge

---
+++ LearnArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] orders = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+        foreach (string order in orders)
+        {
+            if (order.StartsWith("B"))
+            {
+                Console.WriteLine(order);
+            }
+        }
+        {
+            
+        }
+    }
+}
