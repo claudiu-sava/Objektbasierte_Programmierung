b943eeab54504d44dee4ddd42ae0cec4b9ec524a
claudiu-sava <claudiu.sava@protonmail.com>
2023-08-22 08:36:36 +0200

    Worked today

--- LearnArrays/Program.cs
+++ LearnArrays/Program.cs
@@ -6,12 +6,10 @@ class Program
     {
         string[] orders = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
-        foreach (string order in orders)
-        {
-            if (order.StartsWith("B"))
-            {
-                Console.WriteLine(order);
-            }
+        for (int i = 0; i< orders.Length; i++){
+            
+            Console.WriteLine(orders[i]);
+        
         }
     }
 }
